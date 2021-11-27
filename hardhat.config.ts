f78f2e92cbf2732f3147a753e21ea36673805613
Nicolás Bevilacqua <nicobevilacqua@gmail.com>
2021-11-27 16:50:20 -0300

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,78 @@
+import { config } from 'dotenv';
+config();
+
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-waffle';
+import 'hardhat-gas-reporter';
+import 'solidity-coverage';
+import 'hardhat-watcher';
+import { task } from 'hardhat/config';
+import path from 'path';
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+export default {
+  solidity: {
+    version: '0.8.4',
+    settings: {
+      optimizer: {
+        enabled: !!process.env.OPTIMIZER_ENABLED,
+        runs: 1000,
+      },
+    },
+  },
+
+  networks: {
+    hardhat: {
+      initialBaseFeePerGas: 0, // workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+      },
+    },
+
+    rinkeby: {
+      url: process.env.RINKEBY_URL || '',
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    gasPrice: process.env.GAS_PRICE,
+    coinmarketcap: process.env.CMC_KEY,
+    currency: 'USD',
+    outputFile: process.env.TO_FILE ? path.resolve(__dirname, 'gasReporterOutput.json') : undefined,
+  },
+
+  watcher: {
+    compile: {
+      tasks: ['compile'],
+      files: ['./contracts'],
+      verbose: true,
+    },
+
+    test: {
+      tasks: [{ command: 'test', params: { testFiles: ['{path}'] } }],
+      files: ['./test/**/*'],
+      verbose: true,
+    },
+  },
+
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
